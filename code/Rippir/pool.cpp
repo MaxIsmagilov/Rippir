b2bf0b196f197ff44ad7cfc687c581276ff4436b
Max Ismagilov <mismagilov25@students.polytechnic.org>
2023-07-22 10:22:17 -0700

    Block impl visibility

--- code/Rippir/pool.cpp
+++ code/Rippir/pool.cpp
@@ -1,1 +1,14 @@
 #include "pool.hpp"
+
+namespace rippir
+{
+
+void Pool::go(std::size_t N, std::launch policy) { m_pool->go(N, policy); }
+
+void Pool::close() { m_pool->close(); }
+
+void Pool::abort() { m_pool->abort(); };
+
+Pool::Pool() : m_pool{} {}
+
+}  // namespace rippir
