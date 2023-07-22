071580a467b7c2176c7bd0f56da7f2a7133bd0d6
Max Ismagilov <mismagilov25@students.polytechnic.org>
2023-07-21 22:56:42 -0700

    Add lib

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Ripper
-A multithreading utility
+# Rippir
+
+Rippir is a small but powerful multithreading utility made by Blooop.
+Unlike other programs, lamda use is not required, adding ease-of-use and memory securtiy to the library.
---
+++ code/Rippir/pool.hpp
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <atomic>
+#include <condition_variable>
+#include <deque>
+#include <functional>
+#include <future>
+#include <mutex>
+#include <thread>
+#include <any>
+
+namespace rippir
+{
+
+class Pool
+{
+ private:
+  std::mutex m;
+  std::condition_variable v;
+  std::deque<std::packaged_task<void()>> pending;
+  std::vector<std::future<void>> finished;
+
+  void thread_task() {
+    while (true) {
+      std::packaged_task<void()> f;
+      {
+        std::unique_lock<std::mutex> l(m);
+        if (pending.empty()) {
+          v.wait(l, [&] { return !pending.empty(); });
+        }
+        f = std::move(pending.front());
+        pending.pop_front();
+      }
+      if (!f.valid()) {
+        return;
+      }
+      f();
+    }
+  }
+
+ public:
+
+  /// @brief Queue a task
+  /// @tparam F a function
+  /// @tparam ...Params arguments for a function
+  /// @param f a pointer to a function
+  /// @param ...args 
+  /// @return a future to an `any`, this will be the return type of f
+  template <typename F, typename... Params>
+  std::future<std::any> queue(F&& f, Params&&... args) {
+
+    // create a packaged task
+    std::packaged_task<std::any()> p(std::bind(std::forward<F>(f), std::forward<Params>(args)...));
+
+    // get a future from the task
+    auto r = p.get_future();
+
+    // lock the mutex
+    std::unique_lock<std::mutex> l(m);
+
+    // queue the task
+    pending.emplace_back(std::move(p));
+
+    // notify a thread
+    v.notify_one();
+
+    // return the future result of the task 
+    return r;  
+  }
+
+  /// @brief start running N threads
+  /// @param N the number of threads
+  /// @param policy the launch policy, defaulted to async
+  void go(std::size_t N = 1, std::launch policy = std::launch::async) {
+    for (std::size_t i = 0; i < N; ++i) {
+      finished.push_back(std::async(policy, [this] { thread_task(); }));
+    }
+  }
+
+  /// @brief stop all tasks
+  void abort() {
+    cancel_pending();
+    close();
+  }
+
+  /// @brief cancel the queue
+  void cancel_pending() {
+    std::unique_lock<std::mutex> l(m);
+    pending.clear();
+  }
+
+  /// @brief closes the Pool
+  void close() {
+    {
+      std::unique_lock<std::mutex> l(m);
+      for (auto&& unused : finished) {
+        pending.push_back({});
+      }
+    }
+    v.notify_all();
+    finished.clear();
+  }
+
+  ~Pool() { this->close(); }
+
+  Pool() = default;
+  Pool(const Pool& other) = delete;
+  Pool(Pool&& other) = delete;
+  Pool& operator=(const Pool& other) = delete;
+  Pool& operator=(Pool&& other) = delete;
+};
+
+}  // namespace ripper
